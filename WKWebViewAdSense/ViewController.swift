cdc33499a6d6bfdf0dd740bdd2ba347221d6f812
Peter Leung <babyghost@gmail.com>
2017-01-17 01:21:54 +0000

    WKWebView init.

--- WKWebViewAdSense/ViewController.swift
+++ WKWebViewAdSense/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
 
     @IBOutlet weak var internalWebView: UIView!
+    var webView = WKWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let frame = CGRect(x: 0.0, y: 0.0, width: self.internalWebView.bounds.width, height: self.internalWebView.bounds.height)
+        webView.frame = frame
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        self.internalWebView.addSubview(webView)
+        webView.load(URLRequest(url: URL(string: "http://www.9to5mac.com/")!))
     }
 
     override func didReceiveMemoryWarning() {
